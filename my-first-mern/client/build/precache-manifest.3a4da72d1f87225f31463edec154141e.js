419809321d17d8ba1212b37d3cad3a9abba08fd7
Yann Klein <yann.klein@me.com>
2020-05-11 19:56:26 +0900

    change to make it pushable to heroku

---
+++ my-first-mern/client/build/precache-manifest.3a4da72d1f87225f31463edec154141e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c4ad4130ddd875f8ffd7d7239b9033",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9393b343f65245ceab9c",
+    "url": "/static/css/2.eaf15ac0.chunk.css"
+  },
+  {
+    "revision": "9393b343f65245ceab9c",
+    "url": "/static/js/2.1afc47f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1afc47f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5473ef8c949e36021219",
+    "url": "/static/js/main.f1fd17a4.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
