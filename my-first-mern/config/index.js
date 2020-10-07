5283099446a2571c5a4aea9b2de92cf7f85ba0e0
Yann Klein <yann.klein@me.com>
2020-10-07 15:56:49 +0900

    correct this repo folder mayhem

--- my-first-mern/config/index.js
+++
@@ -1,6 +0,0 @@
-const dbuser = "yannklein";
-const dbpassword = "wedges87";
-
-const MONGODB_URI = `mongodb+srv://${dbuser}:${dbpassword}@cluster0-qvxok.gcp.mongodb.net/test?retryWrites=true&w=majority`;
-
-module.exports = MONGODB_URI;
